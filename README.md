e658df9b17051318334dc57ea6427702111982de
Yash Surpaithankar <surpaithankary19@gmail.com>
2026-06-07 17:03:12 +0530

    Updated README.md file

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # TempConverter
 Super basic Python logic to convert the temperature provided by user in Celsius into Fahrenheit.
+This code contain hardcoded values for compitability testing with cd concepts.
--- TempLogic.py
+++ TempLogic.py
@@ -1,5 +1,4 @@
-celsius = float(input("Enter temperature in Celsius: "))
-
-fahrenheit = (celsius * 9/5) + 32
-
-print("Temperature in Fahrenheit is:", fahrenheit)
+celsius = 30
+fahrenheit = (30 * 9/5) + 32
+print("Temperature in Celsius before conversion is 30 degrees")
+print("Temperature in Fahrenheit after conversion  is:", fahrenheit)
